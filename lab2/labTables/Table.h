03e3e8b597ac5eb447a251ef896aad0e19af68c2
SaltySnail <denchikdreams@gmail.com>
2020-05-27 17:48:56 +0300

    +complete work

--- lab2/labTables/Table.h
+++ lab2/labTables/Table.h
@@ -2,6 +2,9 @@
 #include <exception>
 #include <iostream>
 #include <algorithm>
+#include <list>
+#include <string>
+#include <vector>
 
 template<class Key, class Data>
 class Record {
@@ -28,6 +31,12 @@ inline bool Record<Key, Data>::operator==(const Key& _key)
     return key == _key;
 }
 
+template<class Key, class Data>
+std::ostream& operator<<(std::ostream& os, const Record<Key, Data>& d)
+{
+    os << "Key: " << d.key << " Data: " << d.data << std::endl;
+    return os;
+}
 
 template<class Key, class Data>
 class Table {
@@ -37,7 +46,7 @@ protected:
     Record<Key, Data>* dataArray;
 
 public:
-    Table(int _size) : size(_size), count(0)
+    Table(int _size = 10) : size(_size), count(0)
     {
         dataArray = new Record<Key, Data>[size];
     }
@@ -54,6 +63,14 @@ public:
                 return dataArray[i].data;
         throw std::exception("Not found");
     }
+    
+    int getSize() const { return size; }
+    int getCount() const { return count; }
+
+    Record<Key, Data> getRecord(int i) const
+    {
+        return dataArray[i];
+    }
 };
 
 
@@ -63,7 +80,7 @@ class SortTable : public Table<Key, Data >
 public:
     SortTable(size_t _size);
     SortTable(const SortTable<Key, Data>& table);
-    SortTable(const Table<Key, Data>& table); //реализовать любым способом 
+    SortTable(const Table<Key, Data>& table); //done
     void Add(Key key, Data data);
     Data Find(Key key);
     void Delete(const Key& key);
@@ -84,6 +101,20 @@ SortTable<Key, Data>::SortTable(const SortTable<Key, Data>& table) {
     }
 }
 
+template<class Key, class Data>
+SortTable<Key, Data>::SortTable(const Table<Key, Data>& table)
+{
+    this->size = table.getSize();
+    this->count = table.getCount();
+    this->dataArray = new Record<Key, Data>[this->size];
+    for (int i = 0; i < this->count; i++)
+        this->dataArray[i] = table.getRecord(i);
+
+    for (int i = 0; i < this->count; i++)
+        for (int j = 0; j < this->count; j++)
+            if (this->dataArray[i].data > this->dataArray[j].data)
+                std::swap(this->dataArray[i], this->dataArray[j]);
+}
 
 template<class Key, class Data>
 void SortTable<Key, Data>::Add(Key key, Data data) {
@@ -117,3 +148,92 @@ Data SortTable<Key, Data>::Find(Key key) {
     if (this->dataArray[r].key == key) return this->dataArray[r].data;
     throw std::runtime_error("Not Found");
 }
+
+template<class Key, class Data>
+Data& SortTable<Key, Data>::operator[](const Key& key) {
+    int l = 0, r = this->count - 1;
+    while (l + 1 < r)
+    {
+        int mid = (l + r) / 2;
+        if (this->dataArray[mid].key > key)
+            r = mid;
+        else l = mid;
+    }
+    if (this->dataArray[l].key == key) return this->dataArray[l].data;
+    if (this->dataArray[r].key == key) return this->dataArray[r].data;
+    throw std::runtime_error("Not Found");
+}
+
+template<class Key, class Data>
+void SortTable<Key, Data>::Delete(const Key& key)
+{
+    int i = 0;
+    while (i < this->count)
+    {
+        if (this->dataArray[i++].key < key) continue;
+        break;
+    }
+    for (i; i < this->count; i++)
+    {
+        this->dataArray[i].key = this->dataArray[i + 1].key;
+        this->dataArray[i].data = this->dataArray[i + 1].data;
+    }
+}
+
+//------------------------hashTable
+
+int HashFunc(int key, int size)
+{
+    return key % size;
+}
+
+int HashFunc(std::string key, int size)
+{
+    int res = 0;
+    for (int i = 0; i < key.size(); i++)
+        res += key[i];
+    return res % size;
+}
+template<class Key>
+int OwnHashFunc(Key key, int size)
+{
+    // type your function here
+    return 0;
+}
+
+template<class Key, class Data>
+class HashTable
+{
+protected:
+    std::vector<std::list<Record<Key, Data> > > dataVector;
+    int size;
+    int (*hashFunc)(Key, int);
+
+
+public:
+    HashTable(int Size = 10, bool _ownHashFunc = 0) : size(Size)
+    {
+        dataVector.resize(size);
+
+        if (_ownHashFunc)
+            hashFunc = OwnHashFunc;
+        else hashFunc = HashFunc;
+    }
+
+    void Add(Key key, Data data)
+    {
+        int index = hashFunc(key, size);
+
+        dataVector[index].push_back(Record<Key, Data>(key, data));
+    }
+
+    Data Find(Key key)
+    {
+        int index = hashFunc(key, size);
+        typename std::list<Record<Key, Data> >::iterator it = dataVector[index].begin();
+        while (it->key != key)
+            it++;
+        return it->data;
+    }
+    int getSize() const { return size; }
+};
--- lab2/labTables/exception_example.cpp
+++ lab2/labTables/exception_example.cpp
@@ -2,7 +2,10 @@
 // compile with: /EHsc /GR
 #include <iostream>
 #include <typeinfo>
+#include <stdio.h> 
 #include <string>
+#include <time.h>
+#include "Table.h"
 
 using namespace std;
 class PositiveNumber
@@ -17,6 +20,7 @@ public:
 };
 int main()
 {
+    /*
     //string a = "1";
     int a = -1;
     auto& ti1 = typeid(a);
@@ -26,11 +30,34 @@ int main()
     {
         PositiveNumber n(a);
     }
-    catch (exception & e)
+    catch (exception &e)
     {
         cerr << "Caught " << e.what() << endl;
         cerr << "Type " << typeid(e).name() << endl;
-    };
+    };*/
+
+    /*
+    Table<int, int> test1(3);
+    test1.Add(0, 3);
+    test1.Add(1, 1);
+    test1.Add(2, 2);
+    for (int i = 0; i < 3; i++)
+        cout << test1.Find(i) << " ";
+    SortTable<int, int> sortTest1(test1);
+    sortTest1.Delete(1);
+    try { sortTest1.Find(1); }
+    catch (...)
+    {
+        cerr << "\nCaught exeption\n";
+    }
+    cout << sortTest1.Find(0) << endl;*/
+
+    HashTable<string, int> hashtest(10);
+
+    hashtest.Add("first", 11);
+    hashtest.Add("second", 22);
+
+    cout << hashtest.Find("first") << endl;
 }
 /* Output:
 Caught bad locale name
